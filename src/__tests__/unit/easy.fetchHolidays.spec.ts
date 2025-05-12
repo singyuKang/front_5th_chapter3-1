d31273177cb96090f6a682a19ddc4f280ec2bcfb
singyuKang <rkdtlscks123@naver.com>
2025-05-13 00:28:29 +0900

    test : fetchHolidays 테스트코드 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,18 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const result = fetchHolidays(new Date('2025-10-01'));
+    expect(Object.keys(result)).toHaveLength(5);
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const result = fetchHolidays(new Date('2025-02-01'));
+    expect(result).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const result = fetchHolidays(new Date('2025-01-01'));
+    expect(Object.keys(result)).toHaveLength(4);
+  });
 });
