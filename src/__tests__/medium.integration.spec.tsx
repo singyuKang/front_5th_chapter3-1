f3338ed573a75a1af171192932912d95dd6b5eac
singyuKang <rkdtlscks123@naver.com>
2025-05-14 20:03:01 +0900

    test : 기존일정 추가, 수정 테스트코드 작성

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,19 +1,221 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { render, screen, within, act } from '@testing-library/react';
+import { render, screen, within, act, waitFor } from '@testing-library/react';
 import { UserEvent, userEvent } from '@testing-library/user-event';
 import { http, HttpResponse } from 'msw';
 import { ReactElement } from 'react';
-
+import { setupMockHandlerCreation, setupMockHandlerUpdating } from '../__mocks__/handlersUtils';
 import App from '../App';
 import { server } from '../setupTests';
-import { Event } from '../types';
+import { Event, EventForm } from '../types';
+
+const renderApp = () => {
+  return render(
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  );
+};
+
+const initialEvents: Event[] = [
+  {
+    category: '업무',
+    date: '2025-05-04',
+    id: '1',
+    title: '생일',
+    startTime: '10:00',
+    endTime: '11:00',
+    description: '생일이에여',
+    location: '회의실 A',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  },
+];
+beforeEach(async () => {
+  await act(async () => {
+    vi.setSystemTime(new Date('2025-05-01'));
+    // renderApp();
+  });
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 describe('일정 CRUD 및 기본 기능', () => {
   it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
     // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+    const _initialEvents = [...initialEvents];
+    setupMockHandlerCreation(_initialEvents);
+
+    const newEvent: EventForm = {
+      title: '입력한 새로운 일정 정보 추가',
+      date: '2025-05-20',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '입력한 새로운 일정 정보 추가 입니다.',
+      location: '네이버',
+      category: '개인',
+      notificationTime: 10,
+      repeat: {
+        type: 'daily',
+        interval: 1,
+      },
+    };
+
+    render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    );
+
+    const titleInput = screen.getByLabelText('제목');
+    const dateInput = screen.getByLabelText('날짜');
+    const startTimeInput = screen.getByLabelText('시작 시간');
+    const endTimeInput = screen.getByLabelText('종료 시간');
+    const descriptionInput = screen.getByLabelText('설명');
+    const locationInput = screen.getByLabelText('위치');
+    const categoryInput = screen.getByLabelText('카테고리');
+    const repeatCheckbox = screen.getByLabelText('반복 일정');
+    const notificationTimeSelect = screen.getByLabelText('알림 설정');
+    const repeatEventTypeSelect = screen.queryByLabelText('반복 유형');
+    const repeatEventInterval = screen.queryByLabelText('반복 간격');
+    const addEventButton = screen.getByTestId('event-submit-button');
+
+    await userEvent.type(titleInput, newEvent.title);
+    await userEvent.type(dateInput, newEvent.date);
+    await userEvent.type(startTimeInput, newEvent.startTime);
+    await userEvent.type(endTimeInput, newEvent.endTime);
+    await userEvent.type(descriptionInput, newEvent.description);
+    await userEvent.type(locationInput, newEvent.location);
+    await userEvent.selectOptions(categoryInput, newEvent.category);
+    await userEvent.selectOptions(notificationTimeSelect, newEvent.notificationTime.toString());
+    await userEvent.click(repeatCheckbox);
+
+    if (repeatEventTypeSelect) {
+      await userEvent.selectOptions(repeatEventTypeSelect, newEvent.repeat.type);
+    }
+    if (repeatEventInterval) {
+      await userEvent.type(repeatEventInterval, newEvent.repeat.interval.toString());
+    }
+
+    await userEvent.click(addEventButton);
+    const eventList = screen.getByTestId('event-list');
+
+    expect(within(eventList).getByText(newEvent.title)).toBeInTheDocument();
   });
 
-  it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {});
+  it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {
+    const _initialEvents = [...initialEvents];
+    setupMockHandlerUpdating(_initialEvents);
+
+    render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    );
+
+    const updatedEvent: Event = {
+      category: '개인',
+      date: '2025-05-04',
+      id: '1',
+      title: '기존 일정의 세부 정보를 수정해보기',
+      startTime: '10:00',
+      endTime: '13:30',
+      description: '두번째 설명입니다.',
+      location: '회사 근처 식당',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    };
+
+    const eventList = screen.getByTestId('event-list');
+
+    await waitFor(() => {
+      expect(screen.getByText(initialEvents[0].location)).toBeInTheDocument();
+    });
+
+    const editButton = within(eventList).getAllByLabelText(/edit event/i);
+    await userEvent.click(editButton[0]);
+
+    const titleInput = screen.getByLabelText('제목');
+    const dateInput = screen.getByLabelText('날짜');
+    const startTimeInput = screen.getByLabelText('시작 시간');
+    const endTimeInput = screen.getByLabelText('종료 시간');
+    const descriptionInput = screen.getByLabelText('설명');
+    const locationInput = screen.getByLabelText('위치');
+    const categoryInput = screen.getByLabelText('카테고리');
+    const notificationTimeSelect = screen.getByLabelText('알림 설정');
+    const repeatEventTypeSelect = screen.queryByLabelText('반복 유형');
+    const repeatEventInterval = screen.queryByLabelText('반복 간격');
+
+    const addEventButton = screen.getByTestId('event-submit-button');
+
+    expect(titleInput).toHaveValue(initialEvents[0].title);
+    expect(dateInput).toHaveValue(initialEvents[0].date);
+    expect(startTimeInput).toHaveValue(initialEvents[0].startTime);
+    expect(endTimeInput).toHaveValue(initialEvents[0].endTime);
+    expect(descriptionInput).toHaveValue(initialEvents[0].description);
+    expect(locationInput).toHaveValue(initialEvents[0].location);
+    expect(categoryInput).toHaveValue(initialEvents[0].category);
+    expect(notificationTimeSelect).toHaveValue(initialEvents[0].notificationTime.toString());
+
+    if (repeatEventTypeSelect) {
+      expect(repeatEventTypeSelect).toHaveValue(initialEvents[0].repeat.type);
+    }
+    if (repeatEventInterval) {
+      expect(repeatEventInterval).toHaveValue(initialEvents[0].repeat.interval.toString());
+    }
+
+    await userEvent.clear(titleInput);
+    await userEvent.type(titleInput, updatedEvent.title);
+
+    await userEvent.clear(dateInput);
+    await userEvent.type(dateInput, updatedEvent.date);
+
+    await userEvent.clear(startTimeInput);
+    await userEvent.type(startTimeInput, updatedEvent.startTime);
+
+    await userEvent.clear(endTimeInput);
+    await userEvent.type(endTimeInput, updatedEvent.endTime);
+
+    await userEvent.clear(descriptionInput);
+    await userEvent.type(descriptionInput, updatedEvent.description);
+
+    await userEvent.clear(locationInput);
+    await userEvent.type(locationInput, updatedEvent.location);
+
+    await userEvent.selectOptions(categoryInput, updatedEvent.category);
+
+    await userEvent.selectOptions(notificationTimeSelect, updatedEvent.notificationTime.toString());
+
+    if (repeatEventTypeSelect) {
+      await userEvent.selectOptions(repeatEventTypeSelect, updatedEvent.repeat.type);
+    }
+    if (repeatEventInterval) {
+      await userEvent.type(repeatEventInterval, updatedEvent.repeat.interval.toString());
+    }
+
+    await userEvent.click(addEventButton);
+
+    expect(within(eventList).getByText(updatedEvent.title)).toBeInTheDocument();
+    expect(within(eventList).getByText(updatedEvent.date)).toBeInTheDocument();
+    expect(
+      within(eventList).getByText(updatedEvent.startTime, { exact: false })
+    ).toBeInTheDocument();
+    expect(within(eventList).getByText(updatedEvent.endTime, { exact: false })).toBeInTheDocument();
+    expect(within(eventList).getByText(updatedEvent.description)).toBeInTheDocument();
+    expect(within(eventList).getByText(updatedEvent.location)).toBeInTheDocument();
+    expect(
+      within(eventList).getByText(updatedEvent.category, { exact: false })
+    ).toBeInTheDocument();
+    expect(within(eventList).getByText(/10분 전/i)).toBeInTheDocument();
+
+    if (repeatEventTypeSelect) {
+      expect(repeatEventTypeSelect).toHaveValue(updatedEvent.repeat.type);
+    }
+    if (repeatEventInterval) {
+      expect(repeatEventInterval).toHaveValue(updatedEvent.repeat.interval.toString());
+    }
+  });
 
   it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {});
 });
